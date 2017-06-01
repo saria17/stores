4073724123db6f900a482a140e8026f6397acb13
saria17 <snuriyeva2018@ada.edu.az>
2017-06-01 11:10:27 +0400

    roles column added

--- database/migrations/2017_06_01_064654_roleForeignKey.php
+++ database/migrations/2017_06_01_064654_roleForeignKey.php
@@ -13,10 +13,9 @@ class RoleForeignKey extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
