34ea8f0ca7307ee459ce2dcd988d6da227bd9100
saria17 <snuriyeva2018@ada.edu.az>
2017-06-01 11:12:20 +0400

    corrected foreign key rol

--- database/migrations/2017_06_01_071107_role.php
+++ database/migrations/2017_06_01_071107_role.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RoleForeignKey extends Migration
+class Role extends Migration
 {
     /**
      * Run the migrations.
