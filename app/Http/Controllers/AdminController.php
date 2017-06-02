e617fd1a55eacef5a4afc4cb4e70995533d12958
Arsalan <airavani2018@ada.edu.az>
2017-06-02 12:54:50 +0400

    Template Admin Panel ‘Categories’

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
    public function index(Request $request){
-      return view('/AdminViews/adminHomeView');
+      return view('admin.master');
+   }
+   public function categories(Request $request){
+      return view('admin.categories');
    }
 }
---
+++ resources/views/admin/categories.blade.php
@@ -0,0 +1,15 @@
+@extends('admin.master')
+
+@section('main_content')
+<section class="content-header">
+  <h1>
+    Categories
+    <small>Control panel</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li class="active">Categories</li>
+  </ol>
+</section>
+   <h1>This is categories</h1>
+@stop
